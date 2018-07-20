58ae204d8c24b2ce97f9114958c65e4b2fc1edb4
Miranda Howitt <miranda.howitt@gmail.com>
2018-07-19 18:24:34 -0700

    changed to vanillaJS start init game functionality

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-$("document").ready(() => {
+document.addEventListener('DOMContentLoaded', () => {
 
-  let randomStr = 'Leggings stumptown vape umami meggings la croix tote bag enamel pin lyft letterpress knausgaard photo booth disrupt roof party Craft beer art party meh tumeric gluten-free actually four dollar toast plaid Wolf migas distillery humblebrag tilde fanny pack listicle keffiyeh tbh paleo sustainable Plaid four dollar toast truffaut listicle irony mixtape tousled hella locavore put a bird on it poutine Etsy wayfarers kickstarter hammock af'
+  let randomStr = 'Leggings stumptown vape umami meggings lover croix tote bag enamel pin lyft letterpress knausgaard photo booth disrupt roof party Craft beer art party meh tumeric gluten actually four dollar toast plaid Wolf migas distillery humblebrag tilde fanny pack listicle keffiyeh labyrinth paleo sustainable Plaid four dollar toast truffaut listicle irony mixtape tousled hella locavore put bird poutine Etsy wayfarers kickstarter hammock asfuck'
   const randomWords = randomStr.split(' ');
-  let randomNum = Math.random() * (randomWords.length-1) + 1;
-  let hangedWord = randomWords
+  let hangedRandomWord = randomWords[Math.floor(Math.random()*randomWords.length)].split('')
 
-  $(".letters").on('click', (e)=> {
-    alert($(e.target).text())
+  let counter = 0;
+  let turns = 7;
+  let playerResult = [];
+  let playerLtrGuess = [];
+  let lengthOfLines = hangedRandomWord.length;
+
+  let letterContainer = document.querySelector(".letters-container");
+  let hungWord = document.querySelector(".word-container")
+
+  letterContainer.addEventListener('click', (event) => {
+    console.log(event.target.innerText)
   })
+
+  const initGame = () => {
+
+    return hangedRandomWord.forEach((char) => {
+      let charContainer = document.createElement('div');
+      let charText = document.createElement('p')
+      let borderBottom = document.createElement('div');
+      charText.innerText = char;
+      charContainer.className = 'word-container-letter';
+
+      hungWord.appendChild(charContainer);
+      charContainer.appendChild(charText);
+      charContainer.appendChild(borderBottom)
+    } )
+  }
+
+  const isInWord = (ltr) => {
+
+  }
+
+  const wrongGuess = (letter) => {
+
+  }
+
+  const winner = () => {
+
+  }
+
+  const lostGame = () => {
+
+  }
+
+  const replayGame = () => {
+
+  }
+
+
+  initGame()
+
+
+  // letter.addEventListener('click', (e) => {
+  //   alert((e.target).innerText)
+  // })
 
 })
