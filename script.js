11892ee4155420fc6f9ea8d563b877a4687982b3
Miranda Howitt <miranda.howitt@gmail.com>
2018-07-24 15:39:17 -0700

    showing if in word alerting if you've already clicked or not in word

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
   let randomStr = 'Leggings stumptown vape umami meggings lover croix tote bag enamel pin lyft letterpress knausgaard photo booth disrupt roof party Craft beer art party meh tumeric gluten actually four dollar toast plaid Wolf migas distillery humblebrag tilde fanny pack listicle keffiyeh labyrinth paleo sustainable Plaid four dollar toast truffaut listicle irony mixtape tousled hella locavore put bird poutine Etsy wayfarers kickstarter hammock asfuck'
   const randomWords = randomStr.split(' ');
-  let hangedRandomWord = randomWords[Math.floor(Math.random()*randomWords.length)].toLowerCase().split('')
+  let hangedRandomWord = randomWords[Math.floor(Math.random()*randomWords.length)].toUpperCase().split('')
 
-  let counter = 0;
-  let turns = 7;
+  let incorrectGuesses = 0;
+  let turns = 8 - incorrectGuesses;
   const hangManParts = ["stand", "noose", 'head', 'torso', 'leg-1', 'leg-2', 'arm-1', 'arm-2']
   let playerResult = [];
   let playerLtrGuess = {};
@@ -31,27 +32,36 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   letterContainer.addEventListener('click', (event) => {
-    let selectedLtr = event.target.innerText;
+    let selectedLtr = event.target.innerText.toUpperCase()
+    console.log(hangedRandomWord)
+
     if (playerLtrGuess[selectedLtr] !== undefined) {
       alert("You've already hit this")
     } else {
-      if (hangedRandomWord.contains(selectedLtr) && playerResult.indexOf(selectedLtr) === -1) {
+
+      if (hangedRandomWord.indexOf(selectedLtr) !== -1 && playerResult.indexOf(selectedLtr) === -1) {
+        alert("it's in the word")
         isInWord(selectedLtr);
       } else {
-        wrongGuess(selectedLtr);
+        alert("not in word")
+        // wrongGuess(selectedLtr);
       }
+      playerLtrGuess[selectedLtr] = 1;
     }
   })
 
   const isInWord = (ltr) => {
-
+    console.log(ltr)
     let letterToShow = document.getElementsByClassName(`letter-${ltr}`);
-    letterToShow.style.visibility = 'visible';
+    for (let i = 0; i < letterToShow.length; i++) {
+    letterToShow[i].style.visibility = 'visible';
+    }
+    playerResult.push(ltr);
 
   }
 
   const wrongGuess = (letter) => {
-    counter++;
+    incorrectGuesses++;
 
     let hangPartToShow;
 
